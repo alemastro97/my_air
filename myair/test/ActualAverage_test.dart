c8cc1fc0a40b844b02f498284f3c8e371853d627
aristidebordoli <aristide.bordoli@mail.polimi.it>
2020-12-26 21:53:11 +0100

    Test

---
+++ myair/test/ActualAverage_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Calcula testing -', () {
+
+    test('Actual value calculation', () {
+
+      SensorData sensordata;
+
+      sensordata = SensorData('01','test1','test2','test3','test4', 'test5')
+
+      List<SensorData> data_sensor;
+      data_sensor.add(1, )
+
+    })
+  });
+}
